b725cd2b863caed83f4327d344f8b2e18b14f50e
a6iyyu <ffst4irs@gmail.com>
2024-01-09 13:11:49 +0700

    Fixed little things.

--- server/settings.py
+++ server/settings.py
@@ -83,5 +83,5 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'client/static/'
+STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
