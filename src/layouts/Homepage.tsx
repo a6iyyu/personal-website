4f759169cdfd856145ddd14d49340c0320297258
a6iyyu <ffst4irs@gmail.com>
2024-02-01 12:44:31 +0700

    Fixed little things.

---
+++ src/layouts/Homepage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Homepage: React.FC = () => {
+  return null;
+};
--- static/components/Header.tsx
+++ static/components/Header.tsx
@@ -2,4 +2,4 @@ import React from "react";
 
 export const Header: React.FC = () => {
   return null;
-};aa
+};
