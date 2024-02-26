382c8ee1b429291abf2fd61da5848277c139e21e
a6iyyu <ffst4irs@gmail.com>
2024-02-26 21:24:25 +0700

    Almost finished building the home page.

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -2,28 +2,36 @@
   import Dropdown from "./dropdown.svelte";
 </script>
 
-<header class="h-[9.5rem] w-[85%] flex justify-between mx-auto">
+<header class="h-36 w-[85%] flex justify-between mx-auto">
   <section class="h-full w-2/5 flex items-center justify-start">
     <a href="/">
-      <img src="/static/img/logo.png?url" alt="Logo" width={80} />
+      <img
+        src="/static/img/logo.png?url"
+        alt="Logo"
+        width={80}
+        data-aos="fade-in"
+      />
     </a>
   </section>
   <section class="h-full w-2/5 flex items-center justify-end lg:justify-around">
     <a
       href="/about"
       class="hidden lg:flex text-gray-950 text-lg hover:text-gray-800 hover:underline font-semibold transition-all duration-300 ease-in-out"
+      data-aos="fade-in"
     >
       About
     </a>
     <a
       href="/blog"
       class="hidden lg:flex text-gray-950 text-lg hover:text-gray-800 hover:underline font-semibold transition-all duration-300 ease-in-out"
+      data-aos="fade-in"
     >
       Blog
     </a>
     <a
       href="/project"
       class="hidden lg:flex text-gray-950 text-lg hover:text-gray-800 hover:underline font-semibold transition-all duration-300 ease-in-out"
+      data-aos="fade-in"
     >
       Project
     </a>
---
+++ src/layouts/index.svelte
@@ -0,0 +1,47 @@
+<section
+  class="h-[50rem] lg:h-[30rem] w-[85%] flex flex-col-reverse lg:flex-row mx-auto mb-20 mt-8"
+>
+  <div
+    class="h-3/5 lg:h-full w-full lg:w-[55%] flex flex-col items-start justify-center"
+  >
+    <h3
+      class="text-3xl text-gray-950 cursor-default font-medium mx-auto lg:m-0"
+      data-aos="fade-right"
+    >
+      Hello everyone!&nbsp;&#128075;
+    </h3>
+    <br />
+    <h2
+      class="text-4xl text-gray-950 cursor-default font-black mx-auto lg:m-0 text-center"
+      data-aos="fade-right"
+    >
+      I'm Rafi Abiyyu Airlangga
+    </h2>
+    <br />
+    <h4
+      class="text-2xl text-gray-950 cursor-default font-normal text-justify [text-align-last:center] lg:[text-align-last:left] lg:m-0 tracking-wide"
+      data-aos="fade-right"
+    >
+      A college student who is studying to <br class="hidden lg:flex" />
+      become a software engineer one day.
+    </h4>
+    <br />
+    <a
+      href="/about"
+      class="h-fit w-fit mx-auto lg:m-0 bg-slate-950 hover:bg-slate-700 text-slate-50 font-semibold text-lg rounded-lg px-10 py-5"
+      data-aos="fade-right"
+      style="transition: all 0.3s ease-in-out"
+    >
+      Learn About Me
+    </a>
+  </div>
+  <div class="h-2/5 lg:h-full w-full lg:w-[45%] grid place-items-center">
+    <img
+      src=""
+      alt="Myself"
+      width=""
+      class="cursor-default font-semibold text-xl"
+      data-aos="fade-left"
+    />
+  </div>
+</section>
