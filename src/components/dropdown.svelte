520bd684bfc39c796b6913f2a20c9386f2cd4bea
a6iyyu <ffst4irs@gmail.com>
2024-02-26 15:42:54 +0700

    Add dropdown.

---
+++ src/components/dropdown.svelte
@@ -0,0 +1,32 @@
+<script>
+  let open = false;
+  const dropdown = () => {
+    open = !open;
+  };
+  const menus = ["About", "Blog", "Project"];
+</script>
+
+<div class="flex lg:hidden relative">
+  <button
+    class="flex lg:hidden p-2 border-[3px] rounded-sm transition-all duration-300 linear border-slate-950 bg-transparent hover:border-slate-600 hover:text-slate-600 z-20"
+    on:click={dropdown}
+  >
+    <i class="bi bi-list text-4xl"></i>
+  </button>
+  {#if open}
+    <div
+      class={`${open ? "block" : "hidden"} h-fit w-fit fixed bg-slate-50 text-slate-950 rounded-lg z-20`}
+      style="margin: 5rem 0 0 -5.6rem"
+      data-aos={`${dropdown ? "fade-down" : "fade-up"}`}
+    >
+      {#each menus as menu}
+        <a
+          href={`/${menu.toLocaleLowerCase()}`}
+          class="block text-md text-center font-semibold px-12 py-5 rounded-lg hover:bg-slate-200 transition-all duration-300 ease-in-out"
+        >
+          {menu}
+        </a>
+      {/each}
+    </div>
+  {/if}
+</div>
--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,10 +1,14 @@
+<script>
+  import Dropdown from "./dropdown.svelte";
+</script>
+
 <header class="h-[9.5rem] w-[85%] flex justify-between mx-auto">
-  <div class="h-full w-2/5 flex items-center justify-start">
+  <section class="h-full w-2/5 flex items-center justify-start">
     <a href="/">
       <img src="/static/img/logo.png?url" alt="Logo" width={80} />
     </a>
-  </div>
-  <div class="h-full w-fit lg:w-2/5 flex items-center justify-around">
+  </section>
+  <section class="h-full w-2/5 flex items-center justify-end lg:justify-around">
     <a
       href="/about"
       class="hidden lg:flex text-gray-950 text-lg hover:text-gray-800 hover:underline font-semibold transition-all duration-300 ease-in-out"
@@ -23,12 +27,6 @@
     >
       Project
     </a>
-    <div class="flex lg:hidden">
-      <button
-        class="flex lg:hidden p-2 border-[3px] rounded-sm transition-all duration-300 ease-in-out border-slate-950 bg-transparent hover:border-slate-700 hover:text-slate-700"
-      >
-        <i class="bi bi-list text-4xl"></i>
-      </button>
-    </div>
-  </div>
+    <Dropdown />
+  </section>
 </header>
