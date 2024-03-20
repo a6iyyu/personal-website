c5b10ff1a30201455b4cf7ccd3b36327e730ba67
ffst4irs@gmail.com <ffst4irs@gmail.com>
2024-03-20 11:23:24 +0700

    Hmm?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,39 +1,34 @@
 import { defineCollection, z } from "astro:content";
 
 const BlogCollections = defineCollection({
+  type: "content",
   schema: z.object({
-    title: z
-      .string()
-      .max(100, { message: "Title must be 100 characters or less!" }),
-    description: z
-      .string()
-      .max(200, { message: "Title must be 200 characters or less!" }),
+    title: z.string(),
+    description: z.string().max(200, { message: "Title must be 200 characters or less!" }),
     date: z.date(),
-    image: z
-      .object({
+    author: z.string(),
+    image: z.object({
         src: z.string(),
         alt: z.string(),
-      })
-      .optional(),
+      }).optional(),
+    tags: z.enum(["Opinion", "Education", "Entertainment"]),
   }),
 });
 
 const ProjectCollections = defineCollection({
+  type: "content",
   schema: z.object({
-    title: z
-      .string()
-      .max(50, { message: "Title must be 50 characters or less!" }),
-    description: z
-      .string()
-      .max(200, { message: "Title must be 200 characters or less!" }),
+    title: z.string(),
+    description: z.string().max(300, { message: "Title must be 300 characters or less!" }),
     startdate: z.date(),
     enddate: z.date(),
-    image: z
-      .object({
+    producer: z.string().optional(),
+    image: z.object({
         src: z.string(),
         alt: z.string(),
-      })
-      .optional(),
+      }).optional(),
+    source: z.string().optional(),
+    demo: z.string().nullable().default(null).optional(),
   }),
 });
 
